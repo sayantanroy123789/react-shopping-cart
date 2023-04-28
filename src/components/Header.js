01916ccd37c0b74ce6cb5e33b87f169873184d85
sayantan <sayantan.roy123789@gmail.com>
2023-04-28 23:13:46 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+
+import {Link} from "react-router-dom";
+import '../App.css';
+
+function Header(props) {
+    return (
+        <div className='flex shopping-card'>
+            <div onClick={() => props.handleShow(false)} >Shopping Cart App</div>
+            <div onClick={() => props.handleShow(true)}> Cart
+                <sup> {props.count} </sup>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
